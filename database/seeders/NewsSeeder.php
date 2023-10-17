0a073cc3180c2398592687595961f635bff2caf0
WisdomQuest <sofronovpv89@mail.ru>
2023-10-17 09:10:45 +0400

    bd 14 hm 1

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\News;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,17 +15,23 @@ class NewsSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 100; $i++) {
-            $author_id = mt_rand(1, 10);
-            $title = STR::random(mt_rand(3, 15));
-            $text = STR::random(mt_rand(3, 100));
-            $created_at = date('Y-m-d H:i:s');
-            DB::table('news')->insert([
-                'author_id' => $author_id,
-                'title' => $title,
-                'text' => $text,
-                'created_at' => $created_at,
-            ]);
-        }
+
+
+        News::factory()->count(200)->create();
+
+
+//        for ($i = 0; $i < 100; $i++) {
+//            $author_id = mt_rand(1, 10);
+//            $title = STR::random(mt_rand(3, 15));
+//            $text = STR::random(mt_rand(3, 100));
+//            $created_at = date('Y-m-d H:i:s');
+//            DB::table('news')->insert([
+//                'author_id' => $author_id,
+//                'title' => $title,
+//                'text' => $text,
+//                'created_at' => $created_at,
+//            ]);
+//        }
+
     }
 }
